4e3fe36108256ed7c73252ad4f06f3a98d7517d1
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-04 22:10:17 +0600

    added header section

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,21 +1,49 @@
 import HeaderImage from "../../../assets/collegium-banner.jpg";
+import { BiSearchAlt } from "react-icons/bi";
+import { FaAnglesDown } from "react-icons/fa6";
 
 const Header = () => {
   return (
     <header
-      className="hero min-h-[91vh]"
+      className="min-h-[91vh] relative"
       style={{
         backgroundImage: `url(${HeaderImage})`,
         backgroundSize: "cover",
         backgroundPosition: "bottom",
       }}
     >
-      <div className="hero-overlay bg-black bg-opacity-50"></div>
-      <div className="fitting">
-        <div className="text-white w-1/2">
-          <h1 className="text-7xl">Your Gateway to Campus Excellence</h1>
-        </div>
+      <div className="absolute h-full w-full z-[1] bg-black bg-opacity-50"></div>
+      <div className="fitting absolute top-[15%] z-[2] text-white flex flex-col items-center text-center">
+        <h2 className="text-2xl mb-3">
+          Search Your Desired <span className="font-semibold">Campus</span>
+        </h2>
+        <form className="flex w-2/3 p-[2px] pl-5 items-center rounded-full bg-white mb-10">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="w-full text-lg text-black outline-none"
+          />
+          <button className="btn btn-circle border-0 bg-blue-500 hover:bg-blue-600 duration-300  text-white text-2xl">
+            <BiSearchAlt />
+          </button>
+        </form>
+        <h1
+          className="text-7xl font-semibold
+        "
+        >
+          We are <span className="text-blue-500 font-bold">Collegium</span>.
+        </h1>
+        <h2 className="text-5xl">
+          Your Gateway to Campus Excellence
+          <br />
+        </h2>
+        <i className="text-lg text-gray-100 w-1/2 mt-14">
+          &quot;Collegium simplifies the college search and booking process,
+          connecting students with top institutions. Explore colleges, learn
+          about admissions, and book your future—all in one place.&quot;
+        </i>
       </div>
+        <FaAnglesDown className="absolute text-white z-20 text-3xl bottom-4 right-1/2 left-1/2" />
     </header>
   );
 };
