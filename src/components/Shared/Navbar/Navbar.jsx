4e758ca01698002dc9b0479af3d720f6c306f6ba
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-04 19:10:17 +0600

    added navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,15 +1,31 @@
 import { NavLink } from "react-router-dom";
-// import { TbLayoutCollage } from "react-icons/tb";
+import { TbLayoutCollage } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-base-100">
-      <NavLink target="/" className="btn btn-ghost text-xl">
-        {/* <TbLayoutCollage /> */}
-        Collegium
+    <nav className="navbar container mx-auto px-20 bg-base-100 flex items-center justify-between">
+      <NavLink target="/" className="text-3xl font-semibold flex items-center">
+        <TbLayoutCollage className="text-4xl text-blue-500" />
+        Collegium<span className="text-blue-500">.</span>
       </NavLink>
+      <div className="space-x-6 text-lg">
+        <NavLink className="hover:text-blue-500 duration-300" target="/">
+          Home
+        </NavLink>
+        <NavLink className="hover:text-blue-500 duration-300" target="/">
+          Colleges
+        </NavLink>
+        <NavLink className="hover:text-blue-500 duration-300" target="/">
+          Admission
+        </NavLink>
+        <NavLink className="hover:text-blue-500 duration-300" target="/">
+          My College
+        </NavLink>
+      </div>
     </nav>
   );
 };
+
+// Home’, ‘Colleges’, Admission, and ‘My College’
 
 export default Navbar;
