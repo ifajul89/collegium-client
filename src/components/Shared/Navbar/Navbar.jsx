5ef8def9d6ea8cc2d5c045c5a9b587f7bf890566
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-04 23:10:09 +0600

    added responsive design for navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,31 +1,52 @@
 import { NavLink } from "react-router-dom";
 import { TbLayoutCollage } from "react-icons/tb";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
 
 const Navbar = () => {
+  const navItems = (
+    <>
+      <NavLink className="hover:text-blue-500 duration-300" to="/">
+        Home
+      </NavLink>
+      <hr className="md:hidden" />
+      <NavLink className="hover:text-blue-500 duration-300" to="/">
+        Colleges
+      </NavLink>
+      <hr className="md:hidden" />
+      <NavLink className="hover:text-blue-500 duration-300" to="/">
+        Admission
+      </NavLink>
+      <hr className="md:hidden" />
+      <NavLink className="hover:text-blue-500 duration-300" to="/">
+        My College
+      </NavLink>
+    </>
+  );
+
   return (
     <nav className="navbar fitting bg-base-100 flex items-center justify-between">
-      <NavLink to="/" className="text-3xl font-semibold flex items-center">
-        <TbLayoutCollage className="text-4xl text-blue-500" />
+      <NavLink to="/" className="text-2xl md:text-3xl font-semibold flex items-center">
+        <TbLayoutCollage className="text-3xl md:text-4xl text-blue-500" />
         Collegium<span className="text-blue-500">.</span>
       </NavLink>
-      <div className="space-x-6 text-lg">
-        <NavLink className="hover:text-blue-500 duration-300" to="/">
-          Home
-        </NavLink>
-        <NavLink className="hover:text-blue-500 duration-300" to="/">
-          Colleges
-        </NavLink>
-        <NavLink className="hover:text-blue-500 duration-300" to="/">
-          Admission
-        </NavLink>
-        <NavLink className="hover:text-blue-500 duration-300" to="/">
-          My College
-        </NavLink>
+      <div className="dropdown dropdown-end md:hidden">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-circle btn-sm sm:btn-md text-xl bg-blue-500 text-white border-0"
+        >
+          <HiOutlineBars3BottomRight />
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-white rounded-box z-[10] w-[150px] px-3 shadow text-center text-lg space-y-1 after:horizontal"
+        >
+          {navItems}
+        </ul>
       </div>
+      <div className="space-x-6 text-lg hidden md:block">{navItems}</div>
     </nav>
   );
 };
-
-// Home’, ‘Colleges’, Admission, and ‘My College’
 
 export default Navbar;
