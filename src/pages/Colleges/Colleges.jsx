4368f75b0192af5193af8784154dd263ead20dd8
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 03:00:44 +0600

    added colleges page

--- src/pages/Colleges/Colleges.jsx
+++ src/pages/Colleges/Colleges.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import CollegeCard from "../../components/Shared/CollegeCard/CollegeCard";
+
 const Colleges = () => {
+  const [colleges, setColleges] = useState([]);
+
+  useEffect(() => {
+    fetch("colleges.json")
+      .then((res) => res.json())
+      .then((data) => setColleges(data));
+  }, []);
+
+  console.log(colleges);
+
   return (
-    <div>
-      Colleges
+    <div className="fitting space-top-bottom grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {colleges.map((college) => (
+        <CollegeCard college={college} key={college._id}></CollegeCard>
+      ))}
     </div>
   );
 };
