ee808227822ac14e8887855eb34af400a74231b6
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 00:20:33 +0600

    added gallery section

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -1,18 +1,24 @@
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
+
+// import required modules
 import { EffectCards } from "swiper/modules";
-import GraduateImg1 from "../../../assets/graduate1.jpeg"
-import GraduateImg2 from "../../../assets/graduate2.jpeg"
-import GraduateImg3 from "../../../assets/graduate3.jpeg"
-import GraduateImg4 from "../../../assets/graduate4.jpeg"
-import GraduateImg5 from "../../../assets/graduate5.jpeg"
-import GraduateImg6 from "../../../assets/graduate6.jpeg"
-import GraduateImg7 from "../../../assets/graduate7.jpeg"
+
+import GraduateImg1 from "../../../assets/graduate1.jpeg";
+import GraduateImg2 from "../../../assets/graduate2.jpeg";
+import GraduateImg3 from "../../../assets/graduate3.jpeg";
+import GraduateImg4 from "../../../assets/graduate4.jpeg";
+import GraduateImg5 from "../../../assets/graduate5.jpeg";
+import GraduateImg6 from "../../../assets/graduate6.jpeg";
+import GraduateImg7 from "../../../assets/graduate7.jpeg";
 
 const Gallery = () => {
   return (
-    <div className="fitting flex space-top-bottom">
+    <div className="fitting flex items-center justify-around gap-5 space-top-bottom">
       <div className="md:w-1/2 space-y-3">
         <h4 className="text-6xl font-semibold">
           Celebrating <span className="text-blue-500">Success</span>.
@@ -27,16 +33,38 @@ const Gallery = () => {
           through our gallery and meet the faces behind our success stories,
           where dreams became reality and potential was unleashed.
         </p>
+        <p className="text-xl text-gray-600">
+        &quot;Discover the inspiring stories of our exceptional graduates, where excellence meets ambition.&quot;
+        </p>
       </div>
-      <div>
+      <div className="w-1/3">
         <Swiper
           effect={"cards"}
           grabCursor={true}
           modules={[EffectCards]}
           className="mySwiper"
         >
-          <SwiperSlide></SwiperSlide>
-        
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg4} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg5} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg6} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-full rounded-xl" src={GraduateImg7} alt="" />
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
