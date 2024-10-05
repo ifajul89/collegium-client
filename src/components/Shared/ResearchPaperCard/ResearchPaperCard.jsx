a23b55819f931356c9dc8c3ee260bf7861d44f68
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 01:48:48 +0600

    added research card

---
+++ src/components/Shared/ResearchPaperCard/ResearchPaperCard.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
+const ResearchPaperCard = ({ image, name, institute, description }) => {
+  return (
+    <div className="p-2 md:p-4 border-4 border-gray-200 rounded-2xl md:rounded-3xl space-y-1">
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "",
+        }}
+className="h-52 rounded-[10px]"
+      ></div>
+      <h4 className="text-lg lg:text-xl font-semibold">{name}</h4>
+      <h5>
+        <b>Institute:</b> {institute}
+      </h5>
+      <p className="text-sm text-gray-500">{description}</p>
+      <hr />
+      <div className="text-center pt-2">
+
+      <NavLink className="hover:text-blue-500 duration-300 hover:underline" to="/" href="">Learn More</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default ResearchPaperCard;
--- src/pages/Home/ResearchPaper/ResearchPaper.jsx
+++ src/pages/Home/ResearchPaper/ResearchPaper.jsx
@@ -1,9 +1,37 @@
+import ResearchPaperCard from "../../../components/Shared/ResearchPaperCard/ResearchPaperCard";
 import SectionTitle from "../../../components/Shared/SectonTitle/SectionTitle";
+import ResearchImg1 from "../../../assets/research1.jpeg";
+import ResearchImg2 from "../../../assets/research2.jpeg";
+import ResearchImg3 from "../../../assets/research3.jpeg";
 
 const ResearchPaper = () => {
   return (
-    <div>
-      <SectionTitle heading="Groundbreaking Student " headingEnd="Research" subHeading="" />
+    <div className="fitting space-top-bottom">
+      <SectionTitle
+        heading="Groundbreaking Student "
+        headingEnd="Research"
+        subHeading="Explore innovative research projects that are shaping the future brought to you by the brightest minds in academia"
+      />
+      <div className="grid grid-cols-3 gap-5">
+        <ResearchPaperCard
+          image={ResearchImg1}
+          name="Quantum Computing: The Next Frontier in Computational Power"
+          institute="Riverside University"
+          description="Authored by researchers at Riverside University, this paper explores the future of quantum computing and its ability to solve complex problems beyond the capabilities of classical computers. It explains the fundamental principles of quantum mechanics that make this technology possible and discusses its potential impact on industries like cybersecurity, pharmaceutical research, and financial analysis. The study also covers the ongoing challenges in quantum algorithm development and hardware stability"
+        />
+        <ResearchPaperCard
+          image={ResearchImg2}
+          name="Quantum Computing: The Next Frontier in Computational Power"
+          institute="Riverside University"
+          description="Authored by researchers at Riverside University, this paper explores the future of quantum computing and its ability to solve complex problems beyond the capabilities of classical computers. It explains the fundamental principles of quantum mechanics that make this technology possible and discusses its potential impact on industries like cybersecurity, pharmaceutical research, and financial analysis. The study also covers the ongoing challenges in quantum algorithm development and hardware stability"
+        />
+        <ResearchPaperCard
+          image={ResearchImg3}
+          name="Quantum Computing: The Next Frontier in Computational Power"
+          institute="Riverside University"
+          description="Authored by researchers at Riverside University, this paper explores the future of quantum computing and its ability to solve complex problems beyond the capabilities of classical computers. It explains the fundamental principles of quantum mechanics that make this technology possible and discusses its potential impact on industries like cybersecurity, pharmaceutical research, and financial analysis. The study also covers the ongoing challenges in quantum algorithm development and hardware stability"
+        />
+      </div>
     </div>
   );
 };
