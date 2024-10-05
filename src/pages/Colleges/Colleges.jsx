6762b8b668e669979e6dbbcf4f7ec47cf5b3de44
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 03:09:45 +0600

    added responsive design for colleges

--- src/pages/Colleges/Colleges.jsx
+++ src/pages/Colleges/Colleges.jsx
@@ -13,10 +13,20 @@ const Colleges = () => {
   console.log(colleges);
 
   return (
-    <div className="fitting space-top-bottom grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-      {colleges.map((college) => (
-        <CollegeCard college={college} key={college._id}></CollegeCard>
-      ))}
+    <div className="fitting space-top-bottom ">
+      <div className="space-y-1 md:space-y-3 text-center md:text-left mb-5 md:mb-10">
+        <h4 className="text-2xl md:text-5xl font-semibold">
+          Colleges You Can <span className="text-blue-500">Trust</span>.
+        </h4>
+        <h5 className="text-xl md:text-4xl">
+          Find your perfect college match
+        </h5>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {colleges.map((college) => (
+          <CollegeCard college={college} key={college._id}></CollegeCard>
+        ))}
+      </div>
     </div>
   );
 };
