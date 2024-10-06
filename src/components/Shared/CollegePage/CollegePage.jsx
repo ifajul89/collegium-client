cca4036d084973bbca2d622f11f8dd57910732f6
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 20:35:04 +0600

    added responsive design for college page

--- src/components/Shared/CollegePage/CollegePage.jsx
+++ src/components/Shared/CollegePage/CollegePage.jsx
@@ -7,10 +7,10 @@ const CollegePage = () => {
   console.log(_id);
 
   return (
-    <div className="fitting space-top-bottom flex gap-5">
-      <img className="rounded-3xl w-1/2" src={image} alt="" />
+    <div className="fitting space-top-bottom flex flex-col md:flex-row gap-5">
+      <img className="rounded-3xl md:w-1/2" src={image} alt="" />
       <div className="space-y-2">
-        <h3 className="text-4xl font-semibold">{name}</h3>
+        <h3 className="text-3xl md:text-4xl font-semibold">{name}</h3>
         <h4 className="text-gray-700 text-lg">Admission Date: {admission_date}</h4>
         <div>
           <h3 className="font-bold text-xl">Events:</h3>
@@ -18,7 +18,7 @@ const CollegePage = () => {
             {events.map((oneEvent, index) => {
               return (
                 <div
-                  className="p-3 text-xl bg-gray-100 hover:bg-gray-200 border rounded-xl border-gray-300 duration-300 text-gray-800"
+                  className="p-3 md:text-xl bg-gray-100 hover:bg-gray-200 border rounded-xl border-gray-300 duration-300 text-gray-800"
                   key={index}
                 >
                   <h6 className="font-semibold">{oneEvent.event_name}</h6>
@@ -34,7 +34,7 @@ const CollegePage = () => {
             {research_history.map((research, index) => {
               return (
                 <div
-                  className="p-3 text-xl bg-gray-100 hover:bg-gray-200 border rounded-xl border-gray-300 duration-300 text-gray-800"
+                  className="p-3 md:text-xl bg-gray-100 hover:bg-gray-200 border rounded-xl border-gray-300 duration-300 text-gray-800"
                   key={index}
                 >
                   <h6 className="font-semibold">{research.title}</h6>
@@ -44,7 +44,7 @@ const CollegePage = () => {
             })}
           </div>
         </div>
-        <h5 className="font-semibold text-xl">
+        <h5 className="font-semibold md:text-xl">
           Sports:{" "}
           <span className="text-gray-500 font-normal">{sports.join(", ")}</span>
         </h5>
