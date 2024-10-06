cdbb40b8a51737dfd811d0586bc3ba32bfbadbcb
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-06 19:24:53 +0600

    added college card

--- src/components/Shared/CollegeCard/CollegeCard.jsx
+++ src/components/Shared/CollegeCard/CollegeCard.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
+
 // eslint-disable-next-line react/prop-types
 const CollegeCard = ({ college }) => {
-  const { image, name, admission_date, events, research_history, sports } =
+  const { _id, image, name, admission_date, events, research_history, sports } =
     college;
 
   return (
     <div className="p-2 md:p-5 border-4 border-gray-200 rounded-2xl md:rounded-3xl space-y-1">
       <img className="rounded-[10px]" src={image} alt="College Image" />
       <h4 className="text-xl lg:text-3xl font-semibold">{name}</h4>
-      <h5 className="text-gray-500 font-semibold">Admission: {admission_date}</h5>
+      <h5 className="text-gray-500 font-semibold">
+        Admission: {admission_date}
+      </h5>
       <div className="flex gap-1">
         <h5 className="bg-blue-200 border rounded-full border-blue-500 px-2 text-blue-500 md:text-lg">
           {events.length} Events
@@ -20,7 +25,12 @@ const CollegeCard = ({ college }) => {
       <h5 className="text-gray-500">
         Sports: <span>{sports.join(", ")}</span>
       </h5>
-      <button className="btn btn-sm md:btn-md w-full text-lg hover:bg-blue-600 duration-300 text-white bg-blue-500">See Details</button>
+      <Link
+        to={`/colleges/${_id}`}
+        className="btn btn-sm md:btn-md w-full text-lg hover:bg-blue-600 duration-300 text-white bg-blue-500"
+      >
+        See Details
+      </Link>
     </div>
   );
 };
