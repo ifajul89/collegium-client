da2560cd4d1ea95b5517ce979f8b0638ed59bd3f
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-07 14:55:12 +0600

    added fix to navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -23,9 +23,24 @@ const Navbar = () => {
     </>
   );
 
+  const authNavItem = (
+    <>
+      <NavLink className="hover:text-blue-500 duration-300" to="/login">
+        Login
+      </NavLink>
+      <hr />
+      <NavLink className="hover:text-blue-500 duration-300" to="/register">
+        Register
+      </NavLink>
+    </>
+  );
+
   return (
     <nav className="navbar fitting bg-base-100 flex items-center justify-between">
-      <NavLink to="/" className="text-2xl md:text-3xl font-semibold flex items-center">
+      <NavLink
+        to="/"
+        className="text-2xl md:text-3xl font-semibold flex items-center"
+      >
         <TbLayoutCollage className="text-3xl md:text-4xl text-blue-500" />
         Collegium<span className="text-blue-500">.</span>
       </NavLink>
@@ -39,12 +54,25 @@ const Navbar = () => {
         </div>
         <ul
           tabIndex={0}
-          className="dropdown-content menu bg-white rounded-box z-[10] w-[150px] px-3 shadow text-center text-lg space-y-1 after:horizontal"
+          className="dropdown-content menu bg-white rounded-box z-[10] w-[150px] px-3 shadow text-center text-lg space-y-1"
         >
           {navItems}
         </ul>
       </div>
       <div className="space-x-6 text-lg hidden md:block">{navItems}</div>
+      <div className="dropdown dropdown-end">
+        <img
+          className="w-10 rounded-full border-2 border-blue-500 cursor-pointer"
+          src="https://i.pinimg.com/originals/07/33/ba/0733ba760b29378474dea0fdbcb97107.png"
+          tabIndex={0}
+        ></img>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-white rounded-box z-[10] w-[150px] px-3 shadow text-center text-lg space-y-1"
+        >
+          {authNavItem}
+        </ul>
+      </div>
     </nav>
   );
 };
