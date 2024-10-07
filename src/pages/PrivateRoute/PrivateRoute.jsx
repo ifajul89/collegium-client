d9bdb050d2dd4a3266b2f2061949ed6304005a3e
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-08 02:09:31 +0600

    fixed private route

--- src/pages/PrivateRoute/PrivateRoute.jsx
+++ src/pages/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../../components/provider/AuthProvider";
+import { AuthContext } from "../../Providers/AuthProvider";
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
